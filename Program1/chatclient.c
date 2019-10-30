214fd838fb1fa44501c11f070c50cde1df42fe8c
Casey Boettcher <chb@socketbox.com>
2019-10-29 23:46:53 -0400

    Submitting preliminary version.

--- Program1/chatclient.c
+++ Program1/chatclient.c
@@ -5,7 +5,7 @@ Assignment:   Program 1
 Date:         October 29, 2019
 
 Much of this code was influenced by "Beej's Guide to Network Programming." Specfically, pages 17, 
-25, 28, 32, 35, 36, and 45 in the document available from https://beej.us/guide/bgnet/pdf/bgnet_usl_c_1.pdf.
+25, 28, 32-36, and 45 in the document available from https://beej.us/guide/bgnet/pdf/bgnet_usl_c_1.pdf.
 
 Other resources paraphrased or quoted verbatim are noted in context-specific comments within the code.
 */
@@ -21,15 +21,19 @@ Other resources paraphrased or quoted verbatim are noted in context-specific com
 #include <arpa/inet.h>
 #include <regex.h>
 
-#ifndef DEBUG
-#define DEBUG 0
+#ifndef DEBUG1
+#define DEBUG1 0
+#endif
+
+#ifndef DEBUG2
+#define DEBUG2 0
 #endif
 
 
-//max length for user messages is 500 chars, minus handle and prompt
-#define SMSG_MAX 	  488
+//max length for user messages is 500 chars, plus handle
+#define SMSG_MAX 	  513
 //max length for received user messages is 500 chars
-#define RMSG_MAX 	  500
+#define RMSG_MAX 	  513
 //max length for user handle is 10 chars, but we will add "> \0"
 #define HANDLE_LEN 	13
 //tcp protocol is listed as 6 in /etc/protocols on flip
@@ -56,40 +60,34 @@ void clean_quit(int sfd, int e)
 {
   //close the socket
   if(sfd > -1)
-    close(sfd);
+  {
+    if(DEBUG2)
+      fprintf(stderr, "Prior to calling shutdown and close.\n");
+    if( shutdown(sfd, SHUT_RDWR) < 0)
+    {
+      if(DEBUG2)
+      {
+        fprintf(stderr, "After calling shutdown.\n");
+        fprintf(stderr, "Shutdown failed: %s", strerror(errno)); 
+      }
+    }
+    if(DEBUG2)
+        fprintf(stderr, "Prior to calling close.\n");
+    if( close(sfd) < 0)
+    {
+      if(DEBUG2)
+      {
+        fprintf(stderr, "After calling close.\n");
+        fprintf(stderr, "Close failed: %s", strerror(errno)); 
+      }
+    }
+  }
   //print out error
-  fputs(strerror(e), stderr);
+  if(e != 0)
+    fputs(strerror(e), stderr);
   exit(e);
 }
 
-
-/*
- * note: 	wasn't aware of all the heavy lifting that getaddrinfo() does when I wrote this, nor did
- *        I take into account that the user would be supplying a hostname and not an IP :|
- *
- * pre: 	n/a
- * in:		pointer to sockaddr_in and command-line args array
- * out:		n/a
- * post:  sockaddr_in ready for connecting
-void build_socket_struct(struct sockaddr_in *srvr, struct in_addr *ip, char *argv[])
-{
-	//convert IP and check for errors before proceeding
-	//if( inet_pton(AF_INET, argv[1], &(srvr->sin_addr)) )
-	if( inet_pton(AF_INET, argv[1], ip) )
-	{
-    //put address in sockaddr_in
-    srvr->sin_addr = *ip;
-
-		//set address family to Internet
-		srvr->sin_family = AF_INET;
-		
-		//convert port arg to int and put it in netowrk byte order
-		srvr->sin_port = htons(atoi( argv[2] ));	
-		
-		//zero out sin_zero padding
-		memset(&(srvr->sin_zero), '0', 8);
-	}
-}*/
 
 /*
  * pre: 	results struct has been zeroed out
@@ -99,7 +97,7 @@ void build_socket_struct(struct sockaddr_in *srvr, struct in_addr *ip, char *arg
  */
 void build_socket_struct(struct addrinfo **results, char *argv[])
 {
-  if(DEBUG)
+  if(DEBUG1)
     printf("In build socket struct...");
   struct addrinfo cfg;
   memset(&cfg, 0, sizeof(struct addrinfo));
@@ -118,7 +116,7 @@ void build_socket_struct(struct addrinfo **results, char *argv[])
 
 
 /* 
- * pre:   n/a
+ * pre:   char array argument has been filled with null bytes
  * in:    a pointer to a character array
  * out:   n/a
  * post:  the client's handle (with prompt) is in the provided char array
@@ -126,17 +124,19 @@ void build_socket_struct(struct addrinfo **results, char *argv[])
 void get_handle(char *handle)
 {
   printf("What's your handle (maximum of 10 characters)? ");
-  fgets(handle, HANDLE_LEN, stdin);
+  //writes 10 chars to handle and then puts a null byte at the end 
+  fgets(handle, HANDLE_LEN-2, stdin);
   
   //strip trailing newline: https://stackoverflow.com/a/28462221/148680 
   handle[strcspn(handle, "\n")] = 0;
-  if(DEBUG)
+  if(DEBUG1)
     printf("after stcspn");
   //append "> "
   strcat(handle, "> \0");
-  if(DEBUG)
+  if(DEBUG1)
     printf("User's handle: %s", handle);
 }
+
 
 /*
  * pre:   a handle has been selected by the user
@@ -155,10 +155,9 @@ int parse_msg(char *msg, char *handle)
   //the struct for the compiled regex
   regex_t res;
   memset(&res, '0', sizeof(regex_t));
+  //we're passing in the REG_NOSUB flag, so we don't care about these next two vars 
   size_t matchcnt = -1; 
-  //regmatch_t matches[1];
   regmatch_t *matches = NULL;
-  //memset(matches, '0', sizeof(regmatch_t));
 
   if(compres = regcomp(&res, QUIT_PATT, REG_NOSUB) == 0)
   { 
@@ -188,10 +187,10 @@ int parse_msg(char *msg, char *handle)
 }
 
 
-//TODO connection refused needs to error out and exit
 int main(int argc, char *argv[])
 {
-	if(argc != 3 )
+  //check that the user has supplied the correct number of arguments	
+  if(argc != 3 )
 	{
 		usage();
 	}
@@ -201,12 +200,7 @@ int main(int argc, char *argv[])
   memset(handle, '\0', HANDLE_LEN);
   get_handle(handle);
 
-	/*create sockaddr_in struct and populate it
-	struct sockaddr_in sconn;
-  struct in_addr ip4;	
-  memset(&ip4, 0, sizeof(struct in_addr));	
-  memset(&sconn, 0, sizeof(struct sockaddr_in));	
-  */
+  //config socket
   struct addrinfo *results, *h;
   build_socket_struct(&results, argv);
 
@@ -225,13 +219,13 @@ int main(int argc, char *argv[])
       fprintf(stderr, "%s", strerror(errno));	
       continue;
     }
-    if(DEBUG)
+    if(DEBUG1)
       fprintf(stderr, "Connecting to %i", *(h->ai_canonname)); 
     break;
   }
 
   //add one to maximum for null byte
-  char send_msg_buff[SMSG_MAX+1];
+  char send_msg_buff[SMSG_MAX];
   char recv_msg_buff[RMSG_MAX];
   do
   {
@@ -244,18 +238,22 @@ int main(int argc, char *argv[])
     
     if(parse_msg(send_msg_buff, handle))
     {
-      if(DEBUG)
+      if(DEBUG1)
         printf("strlen of send buff: %lu", strlen(send_msg_buff));
       send(sckt, send_msg_buff, strlen(send_msg_buff), 0); 
     }
     else
-      clean_quit(sckt, 1);
+    {
+      if (DEBUG2)
+        fprintf(stderr, "Calling clean_quit.");
+      clean_quit(sckt, 0);
+    }
     
     recv(sckt, recv_msg_buff, RMSG_MAX, 0);
     fprintf(stdout, "%s\n", recv_msg_buff);
   }
   while(sckt);
-  
+    
   return 0;
 }
 
